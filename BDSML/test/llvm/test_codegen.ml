a4e9969de60a0afd8c7721b3d9c79e0635d39d47
LeonidElkin <yolkinleonwork@gmail.com>
2025-03-10 00:25:37 +0300

    feat: llvm chapter 1

--- BDSML/test/llvm/test_codegen.ml
+++ BDSML/test/llvm/test_codegen.ml
@@ -3,16 +3,36 @@ open My_llvm
 open Llvm
 
 let () =
-  let compiled4 =
-    Codegen.compile_program
-      ~verbose:true
-      [ AbsStr_func
-          ( "abs_value"
-          , [ "n" ]
-          , LComplex
-              (CExp_apply ("__op_plus", [ AExp_constant (Const_int 52); AExp_ident "n" ]))
-          )
-      ]
+  let compiled1 =
+    Codegen.compile_aexp
+      (AExp_tuple
+         [ AExp_constant (Const_int 42)
+         ; AExp_constant (Const_char 'c')
+         ; AExp_constant (Const_bool true)
+         ; AExp_constant (Const_string "BDSM")
+         ; AExp_constant Const_unit
+         ])
   in
-  ignore compiled4
+  let compiled2 =
+    Codegen.compile_aexp (AExp_construct ("Some", Some (AExp_constant (Const_int 1))))
+  in
+  let compiled3 =
+    Codegen.compile_aexp
+      (AExp_construct
+         ( "::"
+         , Some
+             (AExp_tuple
+                [ AExp_constant (Const_int 1)
+                ; AExp_construct
+                    ( "::"
+                    , Some
+                        (AExp_tuple
+                           [ AExp_constant (Const_int 2); AExp_construct ("[]", None) ])
+                    )
+                ]) ))
+  in
+  dump_value compiled1;
+  dump_value compiled2;
+  dump_value compiled3;
+  dump_module Llvm_init.my_module
 ;;
